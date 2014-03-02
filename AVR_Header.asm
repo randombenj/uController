92ff962b80f92f6d5b442ce73cb8c7ef01adb625
randombenj <ibenjfa@gmail.com>
2014-03-02 19:32:29 +0100

    update header with interupts

--- AVR_Header.asm
+++ AVR_Header.asm
@@ -20,10 +20,40 @@
 ;- Copyright: Benj Fassbind, Sonneggstrasse 13, 6410 Goldau (2013)
 ;------------------------------------------------------------------------------
 
-;--- Kontrollerangabe ---
+;--- uController ---
 .include "m2560def.inc"
 
-          RJMP     Reset
+;------------------------------------------------------------------------------
+; Interrupt-Adresse-Vectors
+
+        RJMP   Reset            ; Ext-Pin, Power-on, Brown-out,Watchdog Reset
+        RETI                    ; External Interrupt Request 0
+        RETI                    ; External Interrupt Request 1
+        RETI                    ; External Interrupt Request 3
+        RETI                    ; External Interrupt Request 4
+        RETI                    ; External Interrupt Request 5
+        RETI                    ; External Interrupt Request 6
+        RETI                    ; External Interrupt Request 7
+        RETI                    ; Pin Change Interrupt Request 0
+        RETI                    ; Pin Change Interrupt Request 1
+        RETI                    ; Pin Change Interrupt Request 2
+        RETI                    ; Watchdog Time-out Interrupt
+        RETI                    ; Timer/Counter2 Compare Match A
+        RETI                    ; Timer/Counter2 Compare Match B
+        RETI                    ; Timer/Counter2 Overflow
+        RETI                    ; Timer/Counter1 Capture Event
+        RETI                    ; Timer/Counter1 Compare Match A
+        RETI                    ; Timer/Counter1 Compare Match B
+        RETI                    ; Timer/Counter1 Compare Match C
+        RETI                    ; Timer/Counter1 Overflow
+        RETI                    ; Timer/Counter0 Compare Match A
+        RETI                    ; Timer/Counter0 Compare Match B
+        RETI                    ; Timer/Counter0 Overflow
+        RETI                    ; SPI Serial Transfer Complete
+        RETI                    ; USART0 Rx Complete
+        RETI                    ; USART0 Data Register Empty
+        RETI                    ; USART0 Tx Complete
+        RETI                    ; Analog Comparator
 
 ;--- Include-Files ---
 
