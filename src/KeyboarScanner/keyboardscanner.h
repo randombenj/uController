8c9e6f27a78bf7700d37e0bd2295853fe3f1f4d1
Benj Fassbind <randombenj@gmail.com>
2014-09-14 17:01:00 +0200

    Declaration of Matrixkeyboard-Scanner function

---
+++ src/KeyboarScanner/keyboardscanner.h
@@ -0,0 +1,14 @@
+#ifndef KEYBOARDSCANNER_H_
+#define KEYBOARDSCANNER_H_
+
+/**
+ * Scans the matrix keyboard for pressed
+ * switches.
+ * @param port
+ *  The port where the matrix keyboard is
+ * @return
+ *  Returns the scan code of the keyboard
+ */
+uint8_t scan_keyboard(uint8_t *port, uint8_t *port_ddr);
+
+#endif // KEYBOARDSCANNER_H_
