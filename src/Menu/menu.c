fbce68800ae2cb92fb1338576a8efa348142219a
randombenj <randombenj@gmail.com>
2014-12-22 08:47:58 +0100

    Basic menu struture

--- src/Menu/menu.c
+++ src/Menu/menu.c
@@ -1,7 +1,54 @@
 #include <avr/io.h>
 #include "menu.h"
+#include "../Display/c/lcd.h"
+
+menu_t menu[2] = {
+  {
+    .actions = {
+      home_up,
+      home_down,
+      home_left,
+      home_right,
+      home_enter,
+    },
+    .static_text = "      .  .     :        W   [        ]  "
+  },
+  {
+    .actions = {
+      timerselect_up,
+      timerselect_down,
+      timerselect_left,
+      timerselect_right,
+      timerselect_enter,
+    },
+    .static_text = "[ / ]   :   -   :   [ ]         |       "
+  }
+};
+
+uint8_t x_cursor_position = 0;
+uint8_t y_cursor_position = 0;
+view_state_t view_state = VIEW;
 
 int main()
 {
+  lcd_init();
   return 0;
 }
+
+/**
+* HOME MENU:
+*/
+void home_up() {}
+void home_down() {}
+void home_left() {}
+void home_right() {}
+void home_enter() {}
+
+/**
+* TIMERSELECT MENU:
+*/
+void timerselect_up() {}
+void timerselect_down() {}
+void timerselect_left() {}
+void timerselect_right() {}
+void timerselect_enter() {}
--- src/Menu/menu.h
+++ src/Menu/menu.h
@@ -2,11 +2,42 @@
 #define MENU_H_
 
 /**
+ * Function pointer returning void
+ */
+typedef void (*void_func_t)(void);
+
+/**
  * Represents an enumeration of View States
  */
 typedef enum {
  VIEW = 0,
  EDIT = 1
 } view_state_t;
+
+/**
+ * Represents a menu type
+ */
+typedef struct {
+  void_func_t actions[5];
+  char static_text[40 + 1];
+} menu_t;
+
+/**
+ * HOME MENU:
+ */
+void home_up();
+void home_down();
+void home_left();
+void home_right();
+void home_enter();
+
+/**
+ * TIMERSELECT MENU:
+ */
+void timerselect_up();
+void timerselect_down();
+void timerselect_left();
+void timerselect_right();
+void timerselect_enter();
 
 #endif // MENU_H_
